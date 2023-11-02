c3505db3734cbf40206fc773fd12b04c563c3ef1
tawansongsang <tawansongsang.k@gmail.com>
2023-11-02 14:33:29 +0000

    first day notion clone

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+export const Heading = () => {
+    return (
+        <div className="max-w-3xl space-y-4">
+            <h1 className="text-3xl sm:text-5xl md:text6xl font-bold">
+                Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">
+                    Notion Clone
+                </span>
+            </h1>
+            <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+                Notion Clone is the connected workspace hwere <br />
+                better, faster work happens.
+            </h3>
+            <Button>
+                Enter Notion Clone
+                <ArrowRight className="h-4 w-4 ml-2" />
+            </Button>
+        </div>
+    )
+}
